2116a090df38a756f916bd98aeba99c7bb427645
AchroDev <achrodev@proton.me>
2024-09-25 18:55:54 -0500

    Converted the C style string to a const char

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     // Strings are immutable by default and are a fixed block of memory.
     // So you really want to make these a const if you know you aren't going to be modifying the string
-    char *name = "AchroDev"; // Although this is a char * (char pointer), it is not a heap allocated value.
+    const char *name = "AchroDev"; // Although this is a char * (char pointer), it is not a heap allocated value.
 
     std::cout << name << std::endl;
 
