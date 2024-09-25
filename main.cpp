87bc11dd08f2431962c7471a0c3ceffa98e4b0db
AchroDev <achrodev@proton.me>
2024-09-25 18:49:35 -0500

    Added comments and C style string example

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+// Strings in C++ are an array of characters
+
 int main()
 {
+    // Strings are immutable by default and are a fixed block of memory.
+    // So you really want to make these a const if you know you aren't going to be modifying the string
+    char *name = "AchroDev"; // Although this is a char * (char pointer), it is not a heap allocated value.
+
+    std::cout << name << std::endl;
 
     std::cin.get();
 }
